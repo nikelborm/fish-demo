fb2574d28ce6610ce03410d6004d9f606672a28e
nikelborm <r0075301net.r@yandex.ru>
2023-03-01 19:24:43 +0300

    asd

--- backend/src/modules/videoLogs/videoLog.controller.ts
+++ backend/src/modules/videoLogs/videoLog.controller.ts
@@ -1,16 +1,60 @@
-import { Body, Post } from '@nestjs/common';
+import { BadRequestException, Body, Post } from '@nestjs/common';
+import { InjectDataSource } from '@nestjs/typeorm';
 import { ApiController } from 'src/tools';
 import { EmptyResponseDTO } from 'src/types';
+import { DataSource } from 'typeorm';
 
-@ApiController('video')
+@ApiController('videoLog')
 export class VideoLogController {
-  @Post('new')
+  constructor(@InjectDataSource() private dataSource: DataSource) {}
+
+  @Post('createNew')
   // @AllowedFor(AccessEnum.SYSTEM_ADMIN)
   async createVideoLog(
     @Body()
-    body: string,
+    body: {
+      path: string;
+    },
   ): Promise<EmptyResponseDTO> {
-    console.log(body);
-    return body;
+    const date = parseDateFromRawFilePath(body.path);
+    await this.dataSource.query(`
+      insert into videos("cam_id","vid_beg","vid_end")
+      values (1, '${date.toISOString()}', '${date.toISOString()}')
+    `);
+    return {};
   }
+}
+
+function parseDateFromRawFilePath(rawFilePathOfVideo: string): Date {
+  // example of rawFilePathOfVideo is '/home/pi/motion/IPCAM_20230301103014.mp4';
+  const regexpForGettingDateParts =
+    /_(?<year>\d{4})(?<month>\d{2})(?<day>\d{2})(?<hours>\d{2})(?<minutes>\d{2})(?<seconds>\d{2})/;
+
+  const matchResults = rawFilePathOfVideo.match(regexpForGettingDateParts);
+  if (!matchResults?.groups)
+    throw new BadRequestException(
+      'Given http path parameter does not have a date',
+    );
+
+  const groups = matchResults.groups;
+  if (
+    !(
+      groups['year'] &&
+      groups['month'] &&
+      groups['day'] &&
+      groups['hours'] &&
+      groups['minutes'] &&
+      groups['seconds']
+    )
+  )
+    throw new BadRequestException('asd');
+
+  const year = parseInt(groups['year'], 10),
+    monthIndex = parseInt(groups['month'], 10) - 1,
+    day = parseInt(groups['day'], 10),
+    hours = parseInt(groups['hours'], 10),
+    minutes = parseInt(groups['minutes'], 10),
+    seconds = parseInt(groups['seconds'], 10);
+
+  return new Date(year, monthIndex, day, hours, minutes, seconds);
 }
