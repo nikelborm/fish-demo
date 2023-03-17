55c4d492e6cc466b5ae4d903e91defdfc95daf82
milana <m.faizulina.sas@gmail.com>
2023-03-17 13:06:28 +0300

    event CRUD

--- shared/src/types/shared/model/event.model.ts
+++ shared/src/types/shared/model/event.model.ts
@@ -1,6 +1,7 @@
-import type {
+/*import type {
   IEventType,
 } from '.';
+*/
 
 export class IEvent {
   id!: number;
@@ -9,7 +10,7 @@ export class IEvent {
 
   eventTypeId!: number;
 
-  eventType!: IEventType;
+//  eventType!: IEventType;
 
   reservoirId!: number;
 
