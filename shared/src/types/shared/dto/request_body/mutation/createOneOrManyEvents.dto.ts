9ce369afe03b280fe9d8ce54a3e344505915e2bc
milana <m.faizulina.sas@gmail.com>
2023-03-29 10:02:43 +0300

    dto request

--- shared/src/types/shared/dto/request_body/mutation/createOneOrManyEvents.dto.ts
+++ shared/src/types/shared/dto/request_body/mutation/createOneOrManyEvents.dto.ts
@@ -8,6 +8,23 @@ import {
 } from 'class-validator';
 
 export class CreateOneEventRequestDTO {
+  @IsString()
+  eventTypeName!: string;
+
+  @Type(() => Date)
+  @IsDate()
+  completionTime!: Date;
+
+  @IsString()
+  @MinLength(1)
+  @MaxLength(255)
+  description!: string;
+
+  @IsNumber()
+  reservoirId!: number;
+}
+
+export class CreateOneEventDTO {
   @IsNumber()
   eventTypeId!: number;
 
